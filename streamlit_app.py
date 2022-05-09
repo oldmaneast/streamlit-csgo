f9d81d55306b98f6c6369088c83075e340c00ef9
oldmaneast <mehrunesdagon13@gmail.com>
2022-05-09 21:58:00 +0100

    added best-worst

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 import matplotlib.transforms as mtrans
+from heapq import nlargest, nsmallest
 
 
 @st.cache
@@ -23,6 +24,9 @@ if 'Unnamed: 0' in df.columns:
     del df['Unnamed: 0']
 st.dataframe(df)
 
+
+# First chart area, will display a simple boxplot
+# of each of the players in the detailed df
 st.subheader('Player Averages')
 option = st.selectbox('Select stat to view',
                         ("Kills", "Assists", "Deaths", "MVPs", "Score"), index=0)
@@ -36,3 +40,43 @@ for t in ax.get_xticklabels():
     t.set_transform(t.get_transform()+trans)
 fig.tight_layout()
 st.pyplot(fig)
+
+
+# Best/Worst comparison
+# Grab the index of the top or bottom 10 scores of selected
+# players, plot on a scatter chart for comparison
+st.subheader('Best/Worst Comparison')
+names = data['Name'].to_list()
+player1_name = st.selectbox('Select Player 1',
+                                names)
+player1_best_worst = st.select_slider(f'{player1_name} at their best or worst?',
+                                        options=['Best', 'Worst'])
+player2_name = st.selectbox('Select Player 2',
+                                names)
+player2_best_worst = st.select_slider(f'{player2_name} at their best or worst? ',
+                                        options=['Best', 'Worst'])
+
+idx = data.index[data['Name']==player1_name]
+idx2 = data.index[data['Name']==player2_name]
+
+if player1_best_worst == 'Worst':
+    p1_x_data = nsmallest(10, data['Kills'][idx[0]])
+    p1_y_data = nsmallest(10, data['Score'][idx[0]])
+if player1_best_worst == 'Best':
+    p1_x_data = nlargest(10, data['Kills'][idx[0]])
+    p1_y_data = nlargest(10, data['Score'][idx[0]])
+
+if player2_best_worst == 'Worst':
+    p2_x_data = nsmallest(10, data['Kills'][idx2[0]])
+    p2_y_data = nsmallest(10, data['Score'][idx2[0]])
+if player2_best_worst == 'Best':
+    p2_x_data = nlargest(10, data['Kills'][idx2[0]])
+    p2_y_data = nlargest(10, data['Score'][idx2[0]])
+
+fig2, ax2 = plt.subplots()
+ax2 = plt.scatter(p1_x_data, p1_y_data, marker="X")
+ax2 = plt.scatter(p2_x_data, p2_y_data, marker="X")
+plt.xlabel("Kills")
+plt.ylabel("Score")
+plt.legend([player1_name, player2_name])
+st.pyplot(fig2)
